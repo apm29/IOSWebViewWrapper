22e23fe07a41704c6fe03dfb2398bd91cdd67476
apm29 <yjw999wow@163.com>
2021-03-27 11:50:14 +0800

    Initial Commit

---
+++ WebviewWrapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebviewWrapper
+//
+//  Created by ciih on 2021/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebviewWrapper/WebviewWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewWrapperApp.swift
+//  WebviewWrapper
+//
+//  Created by ciih on 2021/3/27.
+//
+
+import SwiftUI
+
+@main
+struct WebviewWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
